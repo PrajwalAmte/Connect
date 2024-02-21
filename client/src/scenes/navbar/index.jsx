c9f1191a206bacf835214b5f6f5b6c37388a364e
PrajwalAmte <prajwalamte@gmail.com>
2024-02-21 22:25:25 +0530

    Final changes before Submission

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Connect
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
